7ff4932c042c240e88dc855d69aaed87d5ad22c6
agnieszkaczarnocka <aga.czarnocka@yahoo.co.uk>
2023-03-14 22:27:55 +0000

    new commit

--- src/main/java/first_git_package/MyGitClass.java
+++ src/main/java/first_git_package/MyGitClass.java
@@ -2,7 +2,8 @@ package first_git_package;
 
 public class MyGitClass {
     public static void main(String[] args) {
-        System.out.println("Version1");
+        System.out.println("Version1" +
+                "");
 
     }
 }
